1b0ac6dc57fa8cba0a6b818c81fc4b07599d4b02
Chew <chew@chew.pw>
2020-06-14 19:07:28 -0500

    add other methods to search result

--- lib/dbio/search_result.rb
+++ lib/dbio/search_result.rb
@@ -10,6 +10,51 @@ class DBio::SearchResult
     @data['user']['slug']
   end
 
+  # @return [String] this result's discord username
+  def discord_username
+    @data['discord']['username']
+  end
+
+  # @return [String] this result's discord discriminator
+  def discord_discriminator
+    @data['discord']['discriminator']
+  end
+
+  # @return [Integer] this result's discord ID
+  def discord_id
+    @data['discord']['id'].to_i
+  end
+
+  # @return [String] this result's discord avatar hash
+  def discord_avatar
+    @data['discord']['avatar']
+  end
+
+  # @return [Boolean] if this user is verified
+  def verified?
+    @data['user']['verified'] == 1
+  end
+
+  # @return [Boolean] if this user is a discord.bio staff member
+  def staff?
+    @data['user']['staff']
+  end
+
+  # @return [Boolean] if this user is a premium user
+  def premium?
+    @data['user']['premium']
+  end
+
+  # @return [Integer] this user's upvotes
+  def upvotes
+    @data['user']['upvotes']
+  end
+
+  # @return [String] this user's description
+  def description
+    @data['user']['description']
+  end
+
   # The user itself, for more detail
   # @return [User] the user
   def user
